73042d15768072ce0bb3cebc76cbcfe369107a03
bashonregardless <harshvardhan0622@gmail.com>
2023-07-11 13:46:11 +0000

    lvim config as in ubuntu

---
+++ lvim_config.lua
@@ -0,0 +1,51 @@
+vim.opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
+vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
+vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2 -- insert 2 spaces for a tab
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.wrap = true -- wrap lines
+
+-- use treesitter folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+lvim.plugins = {
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "G",
+      "Git",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GDelete",
+      "GBrowse",
+      "GRemove",
+      "GRename",
+      "Glgrep",
+      "Gedit"
+    },
+    ft = {"fugitive"}
+  },
+  {
+    "Pocco81/auto-save.nvim",
+    config = function()
+      require("auto-save").setup()
+    end,
+  },
+  {
+    "kevinhwang91/nvim-bqf",
+    ft="qf"
+  },
+  { "ellisonleao/gruvbox.nvim" },
+  { "tpope/vim-repeat" },
+  { "ggandor/leap.nvim",
+    config = function()
+      require('leap').add_default_mappings(true)
+    end,
+  },
+}
+
+
