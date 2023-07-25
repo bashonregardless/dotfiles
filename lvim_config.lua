f1edcb25a57c1edaff282a003b4a92575772b784
Harshvardhan Sharma <harshvardhansharma@Harshvardhans-MacBook-Pro.local>
2023-07-25 12:32:31 +0530

    [Copilot config]: Resolved conflict between copilot and cmp

--- lvim_config.lua
+++ lvim_config.lua
@@ -1,7 +1,12 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
 vim.opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
 vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
-vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 2 -- insert 2 spaces for a tab
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4 -- insert 2 spaces for a tab
 vim.opt.relativenumber = true -- relative line numbers
 vim.opt.wrap = true -- wrap lines
 
@@ -46,6 +51,26 @@ lvim.plugins = {
       require('leap').add_default_mappings(true)
     end,
   },
+  { "github/copilot.vim" }
 }
 
+--- [Refer: https://github.com/LunarVim/LunarVim/issues/1856#issuecomment-954224770]
+--- Use <C-e> to dismiss the cmp completion menu and then use <Tab> to accept the copilot suggestions
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_tab_fallback = ""
+local cmp = require "cmp"
 
+lvim.builtin.cmp.mapping["<Tab>"] = function(fallback)
+  if cmp.visible() then
+    cmp.select_next_item()
+  else
+    local copilot_keys = vim.fn["copilot#Accept"]()
+    if copilot_keys ~= "" then
+      vim.api.nvim_feedkeys(copilot_keys, "i", true)
+    else
+      fallback()
+    end
+  end
+end
+---
